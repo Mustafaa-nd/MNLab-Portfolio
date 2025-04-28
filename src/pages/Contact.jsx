98e2086e073bb276766d660caf944410d0dd29e8
Mustafaa-nd <moustaphand1502@gmail.com>
2025-04-28 18:46:04 +0200

    Contact form fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import emailjs from '@emailjs/browser';
+import * as emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
